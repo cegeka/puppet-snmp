62f5e59117f520155372443d2f1b3d11ede3564f
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-05-07 13:51:49 +0200

    Implemented snmp client module

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,8 +9,43 @@
 # Requires:
 #
 # Sample Usage:
+# class { "snmp":
+#   syslocation => 'Hasselt',
+#   syscontact  => 'unix@example.com',
+# }
 #
-class snmp {
+class snmp($syslocation = '', $syscontact = '') {
 
+  package { 'net-snmp' :
+    ensure  => present,
+  }
+
+  file { '/etc/snmp/snmpd.conf' :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0640',
+    content => template("${module_name}/snmpd.conf.erb"),
+    notify  => Service['snmpd'],
+    require => Package['net-snmp'],
+  }
+
+  file { '/etc/sysconfig/snmpd.options' :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0640',
+    source  => "puppet:///modules/${module_name}/snmpd.options",
+    notify  => Service['snmpd'],
+    require => Package['net-snmp'],
+  }
+
+  service { 'snmpd' :
+    ensure      => running,
+    enable      => true,
+    hasrestart  => true,
+    hasstatus   => true,
+    require     => Package['net-snmp'],
+  }
 
 }
--- spec/classes/snmp_spec.rb
+++ spec/classes/snmp_spec.rb
@@ -3,5 +3,32 @@
 require 'spec_helper'
 
 describe 'snmp' do
+	
+	let (:params) { { :syslocation => 'Hasselt' } }
+	let (:params) { { :syscontact => 'unix@cegeka.be' } }
+	
   it { should contain_class 'snmp' }
+
+	it { should contain_service('snmpd').with_ensure('running') }
+
+  it { should contain_package('net-snmp').with_ensure('present') }
+
+  it 'should create /etc/snmp/snmpd.conf' do
+    should contain_file('/etc/snmp/snmpd.conf').with({
+      'ensure' => 'file',
+			'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0640',
+    })
+  end
+
+  it 'should create /etc/sysconfig/snmpd.options' do
+    should contain_file('/etc/sysconfig/snmpd.options').with({
+      'ensure' => 'file',
+			'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0640',
+    })
+  end
+	
 end
